72222fe5ac1aeb336fc4a985682f54164a6488ec
tgy <18028626345>
2019-01-08 22:03:36 +0800

    test1

--- DemoGithub/Common.cs
+++ DemoGithub/Common.cs
@@ -8,5 +8,19 @@ namespace DemoGithub
 {
     class Common
     {
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
     }
 }
